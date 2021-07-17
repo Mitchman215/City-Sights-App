588bcd7501ae3dbea539b307f67828ae20915516
Mitchell Salomon <mitchell_salomon@brown.edu>
2021-07-17 13:39:47 -0400

    Initial Commit

---
+++ City Sights App/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  City Sights App
+//
+//  Created by Mitchell Salomon on 7/17/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
