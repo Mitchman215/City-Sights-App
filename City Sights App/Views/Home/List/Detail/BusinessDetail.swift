28b9c756d07da4280cae6bf02bf30924491407a5
Mitchell Salomon <mitchell_salomon@brown.edu>
2021-07-20 18:13:01 -0400

    - implement business detail view ui

--- City Sights App/Views/Home/List/Detail/BusinessDetail.swift
+++ City Sights App/Views/Home/List/Detail/BusinessDetail.swift
@@ -12,6 +12,99 @@ struct BusinessDetail: View {
     var business: Business
     
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        
+        VStack (alignment: .leading) {
+            
+            VStack (spacing: 0) {
+                GeometryReader { geo in
+                    // Business Image
+                    let uiImage = UIImage(data: business.imageData ?? Data())
+                    Image(uiImage: uiImage ?? UIImage())
+                        .resizable()
+                        .scaledToFill()
+                        .frame(width: geo.size.width, height: geo.size.height)
+                        .clipped()
+                }
+                .ignoresSafeArea(.all, edges: .top)
+                
+                // Open / closed rectangle
+                ZStack (alignment: .leading) {
+                    Rectangle()
+                        .frame(height: 36)
+                        .foregroundColor(business.isClosed! ? .gray : .blue)
+                    Text(business.isClosed! ? "Closed" : "Open")
+                        .foregroundColor(.white)
+                        .bold()
+                        .padding(.leading)
+                }
+            }
+            
+            
+            Group {
+                // Name of business
+                Text(business.name!)
+                    .font(.title)
+                    .bold()
+                    .padding(.vertical, 5)
+                    
+                
+                // Loop through display address
+                if  business.location?.displayAddress != nil {
+                    ForEach(business.location!.displayAddress!, id: \.self) { str in
+                        Text(str)
+                    }
+                }
+                
+                // Rating
+                Image("regular_\(business.rating ?? 0)")
+            }
+            .padding(.horizontal)
+            
+            Divider()
+            
+            // Phone
+            HStack {
+                Text("Phone: ")
+                    .bold()
+                Text(business.displayPhone ?? "")
+                Spacer()
+                Link("Call", destination: URL(string: "tel\(business.phone ?? "")")!)
+            }
+            .padding()
+            
+            Divider()
+            
+            // Reviews
+            HStack {
+                Text("Reviews: ")
+                    .bold()
+                Text(String(business.reviewCount ?? 0))
+                Spacer()
+                Link("Read", destination: URL(string: "\(business.url ?? "")")!)
+            }
+            .padding()
+            
+            Divider()
+            
+            // get directions button
+            Button {
+                // TODO: Show directions
+            } label: {
+                ZStack {
+                    Rectangle()
+                        .frame(height: 48)
+                        .foregroundColor(.blue)
+                        .cornerRadius(10)
+                    
+                    Text("Get Directions")
+                        .foregroundColor(.white)
+                        .bold()
+                }
+            }
+            .padding()
+
+            
+        }
+        
     }
 }
